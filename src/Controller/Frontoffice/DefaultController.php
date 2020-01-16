426349ef3e37c316ea634a281cb7ccdb7e606c23
Alex Alvarez Gárciga <aagarciga@gmail.com>
2020-01-16 11:13:24 -0300

    Cleaning up home-view and guided counter.

--- src/Controller/Frontoffice/DefaultController.php
+++ src/Controller/Frontoffice/DefaultController.php
@@ -25,66 +25,66 @@ class DefaultController extends AbstractController
             'controller_name' => 'DefaultController',
         ]);
     }
+//
+//    /**
+//     * @Route("/bookings/", name="frontoffice_bookings", methods={"POST"})
+//     */
+//    public function getBookings(BookingRepository $bookingRepository, Request $request){
+//
+//
+//        $start = $request->request->get('start');
+//        $end = $request->request->get('end');
+//        $result = array();
+//
+////        if ($start !== 0 || $end !== 0){
+//
+//            $bookings = $bookingRepository->findByPeriod($start, $end);
+//
+//            foreach ($bookings as  $booking){
+//                $result []= array(
+//                    'title' => 'booking',
+//                    'start' => $booking->getCheckInAt()->format('Y-m-d'),
+//                    'end' => $booking->getCheckOutAt()->modify('+1 day')->format('Y-m-d'),
+//                    'rendering' => 'background'
+//                );
+//            }
+////        }
+//
+//        return new JsonResponse($result);
+//    }
 
-    /**
-     * @Route("/bookings/", name="frontoffice_bookings", methods={"POST"})
-     */
-    public function getBookings(BookingRepository $bookingRepository, Request $request){
-
-
-        $start = $request->request->get('start');
-        $end = $request->request->get('end');
-        $result = array();
-
-//        if ($start !== 0 || $end !== 0){
-
-            $bookings = $bookingRepository->findByPeriod($start, $end);
-
-            foreach ($bookings as  $booking){
-                $result []= array(
-                    'title' => 'booking',
-                    'start' => $booking->getCheckInAt()->format('Y-m-d'),
-                    'end' => $booking->getCheckOutAt()->modify('+1 day')->format('Y-m-d'),
-                    'rendering' => 'background'
-                );
-            }
+//    /**
+//     * @Route("/pax/count/by/days/of/period", name="frontoffice_pax_count_by_days_of_period", methods={"POST"})
+//     */
+//    public function getPaxCountByDaysOfPeriod(BookingRepository $bookingRepository, Request $request){
+//        $result = array();
+//        $start = $request->request->get('start');
+//        $end = $request->request->get('end');
+//
+//
+//        $begin = new DateTime($start);
+//        $end = new DateTime($end);
+//        //$end = $end->modify( '+1 day' );
+//
+//        $interval = new DateInterval('P1D');
+//        /**
+//         * @var DatePeriod
+//         */
+//        $daterange = new DatePeriod($begin, $interval ,$end);
+//
+//        foreach($daterange as $date){
+//            $count = $bookingRepository->countPaxPerDay($date, true);
+//            if ($count > 0){
+//                $result[]= array(
+//                    'title' => $count,
+//                'start' => $date->format('Y-m-d'),
+//                'end' => $date->format('Y-m-d')
+//            );
+//            }
+//
 //        }
-
-        return new JsonResponse($result);
-    }
-
-    /**
-     * @Route("/pax/count/by/days/of/period", name="frontoffice_pax_count_by_days_of_period", methods={"POST"})
-     */
-    public function getPaxCountByDaysOfPeriod(BookingRepository $bookingRepository, Request $request){
-        $result = array();
-        $start = $request->request->get('start');
-        $end = $request->request->get('end');
-
-
-        $begin = new DateTime($start);
-        $end = new DateTime($end);
-        //$end = $end->modify( '+1 day' );
-
-        $interval = new DateInterval('P1D');
-        /**
-         * @var DatePeriod
-         */
-        $daterange = new DatePeriod($begin, $interval ,$end);
-
-        foreach($daterange as $date){
-            $count = $bookingRepository->countPaxPerDay($date, true);
-            if ($count > 0){
-                $result[]= array(
-                    'title' => $count,
-                'start' => $date->format('Y-m-d'),
-                'end' => $date->format('Y-m-d')
-            );
-            }
-
-        }
-
-        return new JsonResponse($result);
-    }
+//
+//        return new JsonResponse($result);
+//    }
 
 }
